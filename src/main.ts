3ab94cb6be5a8e0ef3f905e3a21fa0c3b40221bc
efistokl <mykola.shchetinin@gmail.com>
2021-04-30 14:04:39 +0300

    Add Swagger UI (available at /docs)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('My Mind API')
+    .setDescription('The Qvik assignment - My Mind API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
